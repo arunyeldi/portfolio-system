96bca42d4b15a37367a8140a62233c7d7b6155b3
arunyeldi <arunyeldi@gmail.com>
2026-02-06 23:09:14 +0530

    feat: checked for the component rendering

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,19 +3,13 @@
 import { useState } from "react";
 
 export default function Home() {
+  console.log("Component Rendered");;
+  
   const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button
-        onClick={() => {
-          console.log("Button clicked");
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
     </div>
   );
 }
