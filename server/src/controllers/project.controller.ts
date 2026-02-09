005e61b1ed2f93b64fb031a60538e5b0106cd7b6
arunyeldi <arunyeldi@gmail.com>
2026-02-09 23:16:30 +0530

    feat: add validation for title & techStack input fields

--- server/src/controllers/project.controller.ts
+++ server/src/controllers/project.controller.ts
@@ -3,7 +3,22 @@ import { createProject, getAllProjects } from "../services/project.service";
 
 export const createProjectController = async (req: Request, res: Response) => {
   try {
+    const { title, techStack } = req.body;
+
+    if(!title || title.trim() === "") {
+      return res.status(400).json({
+        message: "Title is required",
+      });
+    }
+
+    if(!Array.isArray(techStack) || techStack.length === 0) {
+      return res.status(400).json({
+        message: "At least one technology is required",
+      });
+    }
+
     const project = await createProject(req.body);
+    
     res.status(201).json(project);
   } catch (error) {
     res.status(500).json({ message: "Failed to create project" });
