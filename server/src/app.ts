72e6bde8015cc476fab71276f17128049e5dbf19
arunyeldi <arunyeldi@gmail.com>
2026-02-11 00:00:01 +0530

    feat: modified app.ts functionality

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import projectRoutes from "./routes/project.route";
 import errorHandler from "./middlewares/errorHandler";
+import AppError from "./utils/AppError";
 
 const app = express();
 
@@ -9,8 +10,10 @@ app.use(express.json());
 // THIS LINE IS CRITICAL
 app.use("/api", projectRoutes);
 
+app.use((req, res, next) => {
+  next(new AppError(`Route ${req.originalUrl} not found`, 404));
+});
+
 app.use(errorHandler);
-
-
 
 export default app;
