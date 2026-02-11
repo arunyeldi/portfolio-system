d978c4f43d67b5ee65bc3a064dfdd044d56d68d7
arunyeldi <arunyeldi@gmail.com>
2026-02-11 23:56:12 +0530

    refactor: shape API responses properly

--- server/src/models/project.model.ts
+++ server/src/models/project.model.ts
@@ -1,36 +1,48 @@
 import mongoose  from "mongoose";
 // import { title } from "node:process";
 
-const ProjectSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const ProjectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    shortDescription: {
+      type: String,
+      required: true,
+    },
+    longDescription: {
+      type: String,
+      required: true,
+    },
+    techStack: {
+      type: [String],
+      required: true,
+    },
+    githubUrl: {
+      type: String,
+      required: true,
+    },
+    liveUrl: {
+      type: String,
+      required: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  shortDescription: {
-    type: String,
-    required: true,
-  },
-  longDescription: {
-    type: String,
-    required: true,
-  },
-  techStack: {
-    type: [String],
-    required: true,
-  },
-  githubUrl: {
-    type: String,
-    required: true,
-  },
-  liveUrl: {
-    type: String,
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    toJSON: {
+      transform: function(doc, ret: any) {
+        ret.id = ret._id?toString();
+        delete ret._id; 
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
 const Project = mongoose.model("Project", ProjectSchema);
 
